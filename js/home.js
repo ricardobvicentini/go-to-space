627735b31c718aa538683eb51588a39fe10ae88b
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2023-07-17 21:15:07 -0300

    Add blur navbar

--- js/home.js
+++ js/home.js
@@ -26,3 +26,8 @@ hamb.addEventListener("click", () => {
         close.style.display = 'block';
     }
 })
+
+window.addEventListener("scroll", function(){
+    let header = document.querySelector("header");
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
