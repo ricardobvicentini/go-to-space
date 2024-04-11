e9a4e5e772c2fc62a6d1a6dcb049991797a94b38
ricardobvicentini <ricardobrunovicentini@uol.com.br>
2024-04-11 11:02:03 -0300

    Update carousel.js

--- js/carousel.js
+++ js/carousel.js
@@ -16,6 +16,32 @@ const setSlider = () => {
     itemActiveOld.classList.remove('active');
   }
   items[active].classList.add('active');
+
+  let dotActiveOld = indicator.querySelector('li.active');
+  if (dotActiveOld) {
+    dotActiveOld.classList.remove('active');
+  }
+  dots[active].classList.add('active');
+  indicator.querySelector('.number').innerText = '0' + (active + 1);
 };
 
-setSlider();
+/* setSlider(); */
+
+nextBtn.onclick = () => {
+  active = active + 1 > lastPosition ? 0 : active + 1;
+  carousel.style.setProperty('--calculation', 1);
+  setSlider();
+};
+
+prevBtn.onclick = () => {
+  active = active - 1 < firstPosition ? lastPosition : active - 1;
+  carousel.style.setProperty('--calculation', -1);
+  setSlider();
+};
+
+dots.forEach((item, position) => {
+  item.onclick = () => {
+    active = position;
+    setSlider();
+  };
+});
